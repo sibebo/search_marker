0e306f3703cf517e77db168c8f09e460a1706676
Simon <sibebo@gmail.com>
2023-03-28 21:44:51 +0200

    Using SearchMarker::Get

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main()
     try
     {
         std::string s{"skal skolen udskammes for al skam i verden?"};
+        
+        // Chaining:
         search_marker::SearchMarker()
                        .AddToken("sk")
                        .AddToken("ka")
@@ -13,6 +15,20 @@ int main()
                        .Process(s)
                        .Print();
         std::cout << s << std::endl;
+
+        // Using SearchMarker::Get to extract the processed matches:
+        auto    searcher = search_marker::SearchMarker();
+        searcher.AddTokens({"sk", "ka", "skolen"});
+        
+        searcher.Process(s);
+
+        for (auto [from, to] : searcher.Get())
+        {
+            std::cout << from 
+                      << '\t' << to 
+                      << '\t' << s.substr(from, to - from) 
+                      << std::endl;
+        }
     }
     catch(const std::exception& e)
     {
